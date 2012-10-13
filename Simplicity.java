61cf6fe4b0ccafa86b96e56a5de0e9311b85a72c
Bart Massey <bart@cs.pdx.edu>
2012-10-13 00:42:24 -0700

    started to write actual Java code

--- Simplicity.java
+++ Simplicity.java
@@ -11,12 +11,38 @@
 
 class Coord {
     int r, c;   // row, column coordinates
+
+    Coord(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+    Coord offset(Coord q) {
+        return new Coord(q.r + r, q.c + c);
+    }
+
+    boolean equals(Coord q) {
+        return q.r == r && q.c == c;
+    }
 }
 
 class Block {
     Coord pos;  // coordinates of upper-left corner
     char color;
     Coord[] squares;  // array of relative positions
+
+    Block(Coord pos, char color, Coord[] squares) {
+        this.pos = pos;
+        this.color = color;
+        this.squares = squares;
+    }
+
+    boolean squareAt(Coord pos) {
+        for (int i = 0; i < squares.length; i++)
+            if (this.pos.offset(squares[i]).equals(pos))
+                return true;
+        return false;
+    }
 }
 
 /* Operation on State:
